27e380ba67e2142403ac9ddcc0b6995c3f70d1d8
kenzo2407 <leandro.uehara@sptech.school>
2026-05-29 23:29:57 -0300

    atualizações do site

--- web-data-viz/web-data-viz/src/models/dashboardModel.js
+++ web-data-viz/web-data-viz/src/models/dashboardModel.js
@@ -3,13 +3,15 @@ var database = require("../database/config")
 function dashboard(){
     
     var instrucaoSql = `
-    SELECT
+   SELECT
         pontos AS pontuação,
         DATE_FORMAT(dataR, '%d/%m %H:%i') AS momento_grafico,
-        COUNT(*) AS quantidade
-    FROM quiz
+        nome,
+        conhecimento
+    FROM quiz JOIN usuario
+    ON idUsuario=fkUsuario
     WHERE dataR IS NOT NULL
-    GROUP BY momento_grafico, pontos
+    GROUP BY momento_grafico, pontos, conhecimento, nome
     ORDER BY momento_grafico DESC;
 `;
     return database.executar(instrucaoSql);
